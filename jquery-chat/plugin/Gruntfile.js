1e17a2f2a1442b5f830f0e907c6fb05f86df06e1
Samping Chuang <sampingchuang@gmail.com>
2015-03-22 13:51:07 -0700

    add basic mockup for chat side bar

---
+++ jquery-chat/plugin/Gruntfile.js
@@ -0,0 +1,81 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+   'use strict';
+
+   var BUILD_JS_DIR  = 'dist/'
+
+   // ===========================================================================
+   // CONFIGURE GRUNT ===========================================================
+   // ===========================================================================
+   grunt.initConfig({
+
+      // get the configuration info from package.json ----------------------------
+      // this way we can use things like name and version (pkg.name)
+      pkg: grunt.file.readJSON('package.json'),
+
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ',
+
+      // all of our configuration will go here
+      less: {
+         build: {
+            files: {
+               'dist/jquery.chat.css': 'less/main.less'
+            }
+         },
+      },
+      // configure cssmin to minify css files ------------------------------------
+      cssmin: {
+         options: {
+            banner: '<%= banner %>'
+         },
+         build: {
+            files: {
+               'dist/jquery.chat.min.css': 'dist/jquery.chat.css'
+            }
+         }
+      },
+      concat: {
+         options: {
+            banner: '<%= banner %>',
+            stripBanners: true
+         },
+         dist: {
+            src: 'src/*.js',
+            dest: 'dist/<%= pkg.name %>.js'
+         }
+      },
+      uglify: {
+         options: {
+            banner: '<%= banner %>'
+         },
+         dist: {
+            src: '<%= concat.dist.dest %>',
+            dest: 'dist/<%= pkg.name %>.min.js'
+         }
+      }
+   });
+
+   // ===========================================================================
+   // LOAD GRUNT PLUGINS ========================================================
+   // ===========================================================================
+   // we can only load these if they are in our package.json
+   // make sure you have run npm install so our app can find these
+   grunt.loadNpmTasks('grunt-contrib-less');
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-concat');
+
+   // ===========================================================================
+   // CREATE TASKS ==============================================================
+   // ===========================================================================
+   grunt.registerTask('css', ['less', 'cssmin']);
+
+
+   //grunt.registerTask('compile', ['css','concat','uglify'])
+
+};
