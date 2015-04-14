3497bcb68c954bd1ae0f589b6f5dc0cd4669609a
Samping Chuang <sampingchuang@gmail.com>
2015-04-14 11:26:53 -0700

    remove grunt

--- freelancer-grunt/source/js/app.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var app = angular.module('gemStore', ['store-directives']);
-
-    app.controller('StoreController', ['$http', function($http){
-        var store = this;
-    } ]);
-})();
